dbd7e27ebd87efcbfc85f0302352d6e4d993ee57
bangnokia <bangnokia@gmail.com>
2022-05-10 16:31:30 +0700

    work in progress

---
+++ android/app/src/main/java/com/epichotpot/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.epichotpot.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.epichotpot.app',
+  appName: 'Epic HOTPOT',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
