355779ca24e1f2147c631a8e143ef0259664e601
bangnokia <bangnokia@gmail.com>
2022-05-10 15:22:19 +0700

    work in progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Button from "./Button.svelte";
     import FireCircle from "./FireCircle.svelte";
+    import { fade } from "svelte/transition";
 
     let temp = 0;
     let step = 100;
@@ -13,14 +14,18 @@
     }
 </script>
 
-<main class="w-full h-screen flex flex-col justify-between bg-gray-900">
-    <div class="pt-10 w-250px h-250px flex items-center justify-center mx-auto">
+<main class="font-mono w-full h-screen flex flex-col justify-around bg-gray-900">
+    <div class="h-1/3 shrink-0 grow-0 pt-10 w-250px h-250px flex items-center justify-center mx-auto">
         <FireCircle value={temp} {step} />
     </div>
     <div class="flex items-center justify-center">
-        <strong class="text-7xl font-bold text-rose-500 text-center lining-nums">
-            {temp}
-        </strong>
+        <div class="text-7xl font-bold text-center lining-nums">
+            {#if temp > 0}
+                <span class="text-red-500" transition:fade>{temp}</span>
+            {:else}
+                <span class="text-white">--</span>
+            {/if}
+        </div>
     </div>
     <div class="pb-10 flex items-center justify-center gap-5">
         <Button on:click={down}>-</Button>
--- src/FireCircle.svelte
+++ src/FireCircle.svelte
@@ -11,17 +11,10 @@
         .join(", ");
 </script>
 
-<div class="w-48 h-48 rounded-full border-2 border-gray-500 flex items-center justify-center">
+<div class="w-48 h-48 rounded-full border-2 border-white/10 flex items-center justify-center">
     {#if value}
-        <div class="bg-transparent opacity-50" style="width: {width}px; height: {width}px;">
-            <section style="box-shadow: {shadowString}" class="animate animate-pulse w-full h-full rounded-full  transform transition-transform duration-500" />
+        <div class="bg-transparent opacity-50" style="width: {width}px; height: {width}px; max-width: 169px; max-height: 169px;">
+            <section style="box-shadow: {shadowString}; background: red" class="animate animate-pulse w-full h-full rounded-full transform transition transition-all duration-500" />
         </div>
     {/if}
 </div>
-
-<style>
-    section {
-        background: rgba(255, 0, 0);
-        /* box-shadow: 0 0 10px red, 0 0 20px red, 0 0 30px red, 0 0 40px red, 0 0 250px red, 0 0 300px red; */
-    }
-</style>
